0dab797cf21e54c0d3b05a5098a571a52081922d
PeterL93 <Peter_sl5@hotmail.com>
2018-03-19 14:00:06 +0100

    Ball THINGY HAHAHAHAHAHA

---
+++ BallThingy/Assets/PlayerControl.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerControl : NetworkBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+	// Use this for initialization
+	void Start () {
+
+
+		
+	}
+
+    [Command]
+    void CmdFire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody2D>().AddForce(new Vector2(200, 0));
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        if(!isLocalPlayer)  {
+            return;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(x, 0, 0);
+        transform.Translate(x, z, 0);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+
+}
